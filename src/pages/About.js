cbd05adf6590ce89e6d69af7e6907b174186a8a5
vimal-2705 <78398081+vimal-2705@users.noreply.github.com>
2021-06-19 13:03:07 -0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className="section about-section">
+            <h2 className="section-title">about us</h2>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, animi. Iusto minima explicabo a minus nihil architecto autem ut, mollitia voluptatum eum praesentium numquam eius incidunt repudiandae distinctio dicta voluptatem. Dolores pariatur, mollitia nulla fugiat et consequatur quae officia hic sequi vel recusandae laboriosam commodi. </p>
+        </section>
+    )
+}
+
+export default About
